464e9a3d8cab9512655beba2d1c2bfedc151fdae
José Naves <josenaves@gmail.com>
2018-02-16 08:13:46 -0200

    Add API client classes (Retrofit)

---
+++ app/src/main/java/com/josenaves/acviewmodel/networking/RepoApi.java
@@ -0,0 +1,36 @@
+package com.josenaves.acviewmodel.networking;
+
+
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+public class RepoApi {
+    private static final String BASE_URL = "https://api.github.com/";
+
+    private static Retrofit retrofit;
+    private static RepoService repoService;
+
+    public static RepoService getInstance() {
+        if (repoService != null) {
+            return repoService;
+        }
+
+        if (retrofit == null) {
+            initializeRetrofit();
+        }
+
+        repoService = retrofit.create(RepoService.class);
+        return repoService;
+    }
+
+    private static void initializeRetrofit() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build();
+
+    }
+
+    private RepoApi() {
+    }
+}
---
+++ app/src/main/java/com/josenaves/acviewmodel/networking/RepoService.java
@@ -0,0 +1,13 @@
+package com.josenaves.acviewmodel.networking;
+
+import com.josenaves.acviewmodel.model.Repo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RepoService {
+    @GET("orgs/Google/repos")
+    Call<List<Repo>> getRepositories();
+}
