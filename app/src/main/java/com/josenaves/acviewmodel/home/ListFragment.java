9b7b346f4b5af9ddab128c17781b8583765db69e
José Naves <josenaves@gmail.com>
2018-02-16 09:17:57 -0200

    Add recyclerview to fragment

--- app/src/main/java/com/josenaves/acviewmodel/home/ListFragment.java
+++ app/src/main/java/com/josenaves/acviewmodel/home/ListFragment.java
@@ -1,8 +1,12 @@
 package com.josenaves.acviewmodel.home;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +15,9 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.josenaves.acviewmodel.R;
+import com.josenaves.acviewmodel.model.Repo;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +30,7 @@ public class ListFragment extends Fragment {
     @BindView(R.id.loading_view) ProgressBar loadingView;
 
     private Unbinder unbinder;
+    private ListViewModel viewModel;
 
     @Nullable
     @Override
@@ -30,6 +38,43 @@ public class ListFragment extends Fragment {
         View view = inflater.inflate(R.layout.screen_list, container, false);
         unbinder = ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        viewModel = ViewModelProviders.of(this).get(ListViewModel.class);
+
+        listView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        listView.setAdapter(new RepoListAdapter(viewModel, this));
+        listView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        observeViewModel();
+    }
+
+    private void observeViewModel() {
+        viewModel.getRepos().observe(this, repos -> {
+            if (repos != null) {
+                listView.setVisibility(View.VISIBLE);
+            }
+        });
+        viewModel.getError().observe(this, isError -> {
+            if (isError) {
+                errorTextView.setVisibility(View.VISIBLE);
+                listView.setVisibility(View.GONE);
+                errorTextView.setText(R.string.api_error_repos);
+            } else {
+                errorTextView.setVisibility(View.GONE);
+                errorTextView.setText(null);
+            }
+        });
+        viewModel.getLoading().observe(this, isLoading -> {
+            loadingView.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+            if (isLoading) {
+                errorTextView.setVisibility(View.GONE);
+                listView.setVisibility(View.GONE);
+
+            }
+        });
     }
 
     @Override
